f0a3664f3e2fee97ee21df5e888cf844f7f8f4bf
ROGER KOADERE <rogerkoadere@gmail.com>
2026-08-30 14:15:08 +0200

    Grande MAJ db

--- database.py
+++ database.py
@@ -11,7 +11,13 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 if not SQLALCHEMY_DATABASE_URL:
     raise ValueError("L'URL de la base de données (DATABASE_URL) n'est pas configurée dans le fichier .env")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Ajout des paramètres pour éviter les erreurs "SSL SYSCALL error: EOF detected" sur Render
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
